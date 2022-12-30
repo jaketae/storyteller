9338daa2fab9a6c946ada85b8a5e7d69940858a1
Jake Tae <jaesungtae@gmail.com>
2022-12-30 19:35:50 +0900

    docs: add comments in utils helper functions

--- storyteller/utils.py
+++ storyteller/utils.py
@@ -8,29 +8,34 @@ import torch
 
 
 def check_ffmpeg():
+    """Check ffmpeg installation."""
     if shutil.which("ffmpeg") is None:
         raise RuntimeError("`ffmpeg` not found. Please install `ffmpeg` and try again.")
 
 
 def make_timeline_string(start, end):
+    """Create timeline string to write onto .srt subtitle files."""
     start = format_time(start)
     end = format_time(end)
     return f"{start} --> {end}"
 
 
 def format_time(time):
+    """Transform time (seconds) to .srt format."""
     mm, ss = divmod(time, 60)
     hh, mm = divmod(mm, 60)
     return f"{hh:02d}:{mm:02d}:{ss:02d},000"
 
 
 def subprocess_run(command):
+    """Wrapper around `subprocess.run()` with /dev/null redirection in stdout and stderr."""
     subprocess.run(
         command.split(), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
     )
 
 
 def set_seed(seed):
+    """Set seed."""
     random.seed(seed)
     os.environ["PYTHONHASHSEED"] = str(seed)
     np.random.seed(seed)
