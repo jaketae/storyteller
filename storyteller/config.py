da9c861a916c937be682b6b14f8384bfb84a6907
Christopher Woodall <woodall.christopher@gmail.com>
2023-01-03 19:47:38 -0500

    update output path
    should probably be relative to where the command was called from, not the directory the code resides.

--- storyteller/config.py
+++ storyteller/config.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from dataclasses import dataclass
 
 
@@ -10,6 +11,6 @@ class StoryTellerConfig:
     speaker: str = "tts_models/en/ljspeech/glow-tts"
     writer_device: str = "cuda:0"
     painter_device: str = "cuda:0"
-    output_dir: str = "out"
+    output_dir: str = Path(__file__).parent / "out"
     seed: int = 42
     diffusion_prompt_prefix: str = "Beautiful painting"
