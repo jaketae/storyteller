a7b04a0a3af6b547f7c29c3ba9b4764c3954c358
Jake Tae <jaesungtae@gmail.com>
2023-01-11 02:53:36 +0900

    fix: replace __package__ w/ storyteller-core

--- src/storyteller/__init__.py
+++ src/storyteller/__init__.py
@@ -3,5 +3,5 @@ from importlib import metadata
 from storyteller.config import StoryTellerConfig
 from storyteller.model import StoryTeller
 
-__version__ = metadata.version(__package__)
+__version__ = metadata.version("storyteller-core")
 __all__ = ["__version__", "StoryTellerConfig", "StoryTeller"]
