95e948bdb47ac7f9b6ea48ab94c90bb47437e1ba
Jake Tae <jaesungtae@gmail.com>
2023-01-11 01:46:17 +0900

    chore: add version to init

--- src/storyteller/__init__.py
+++ src/storyteller/__init__.py
@@ -1,2 +1,7 @@
-from storyteller.config import StoryTellerConfig  # noqa: F401
-from storyteller.model import StoryTeller  # noqa: F401
+from importlib import metadata
+
+from storyteller.config import StoryTellerConfig
+from storyteller.model import StoryTeller
+
+__version__ = metadata.version(__package__)
+__all__ = ["__version__", "StoryTellerConfig", "StoryTeller"]
